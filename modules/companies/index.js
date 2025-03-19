e6c98d12f9339fca6223f12cba2e58add426c869
Alessandro Urpi <aurpi@bitfarm.com.mx>
2025-03-19 16:24:33 -0600

    del codigo generado

--- modules/companies/index.js
+++ modules/companies/index.js
@@ -2,20 +2,5 @@ const { BaseModule } = helpers;
 
 const myself = new BaseModule("companies");
 
-// Register services
-myself.exportServices = (services) => {
-    services.companyService = require('./services/company/company-service');
-    services.companyStatsService = require('./services/company/company-stats-service');
-};
-
-// Register virtual models
-myself.exportModels = (models) => {
-    // Register the CompanyStats virtual model
-    const { sequelize } = helpers;
-    models.CompanyStats = require('./models/company-stats')(
-        sequelize.sequelize, 
-        sequelize.Sequelize.DataTypes
-    );
-};
 
 module.exports = myself;
