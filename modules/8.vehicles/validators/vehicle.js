bdf77be6226f2cf3aea90e3682d1882721854571
zmendoza <zmendoza@bitfarm.com.mx>
2025-05-14 18:15:58 -0600

    se agrego validaciones para state & vehicleYear

--- modules/8.vehicles/validators/vehicle.js
+++ modules/8.vehicles/validators/vehicle.js
@@ -1,6 +1,30 @@
 const { CRUDValidator } = helpers;
 const { vehicle } = models;
+const Joi = require('joi');
+class VehicleValidator extends CRUDValidator {
+    constructor() {
+        super(vehicle);
 
-class VehicleValidator extends CRUDValidator {}
+        this.addFieldValidation(
+            'status',
+            Joi.string()
+                .valid('available', 'unavailable', 'maintenance')
+                .required()
+                .messages({
+                    'any.required': i18n.__('error.validation.required', 'status'),
+                    'any.only': i18n.__('error.vehicle.validation.status.invalid', 'status'),
+                }),
+        );
+        this.addFieldValidation(
+            'vehicleYear',
+            Joi.number()
+                .integer()
+                .min(1980)
+                .max(2100)
+                .message('El campo vehicleYear debe ser un número de 4 dígitos (por ejemplo, 2025)')
+                .optional(),
+        );
+    }
+}
 
-module.exports = new VehicleValidator(vehicle);
+module.exports = new VehicleValidator();
