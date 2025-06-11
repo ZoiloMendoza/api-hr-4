f63623241bb6871d358b6e62054b69f492fe4509
Jose Resendiz <resendizj607@gmail.com>
2025-06-11 16:39:40 -0600

    modulo de mantenimiento

--- modules/11.audit/models/auditlog.js
+++ modules/11.audit/models/auditlog.js
@@ -58,6 +58,7 @@ module.exports = (sequelize, DataTypes) => {
         {
             sequelize,
             modelName: 'Auditlog',
+            tableName: 'AuditLogs'
         },
     );
     return Auditlog;
